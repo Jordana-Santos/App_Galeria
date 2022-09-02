2f295f67caf2fae99423655bf8caa2cf034c7850
20201TIIMI0349 <jordanalsantos@hotmail.com>
2022-09-02 12:04:50 -0300

    passo 14

--- app/src/main/java/santos/jordana/galeria/MainAdapter.java
+++ app/src/main/java/santos/jordana/galeria/MainAdapter.java
@@ -2,6 +2,7 @@ package santos.jordana.galeria;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -24,11 +25,15 @@ public class MainAdapter extends RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // Irá inflar o Layout.
+        LayoutInflater inflater = LayoutInflater.from(mainActivity);
+        // Constroi uma view baseado nas regras do arquivo e o armazena.
+        View v = inflater.inflate(R.layout.item_list, parent, false);
+        return new MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
         ImageView imPhoto = holder.itemView.findViewById(R.id.imItem);
         int w = (int) mainActivity.getResources().getDimension(R.dimen.itemWidth);
         int h = (int) mainActivity.getResources().getDimension(R.dimen.itemHeight);
@@ -41,6 +46,7 @@ public class MainAdapter extends RecyclerView.Adapter {
             }
         });
     }
+
 
 
     @Override
--- app/src/main/java/santos/jordana/galeria/PhotoActivity.java
+++ app/src/main/java/santos/jordana/galeria/PhotoActivity.java
@@ -4,13 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    String photoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,14 @@ public class PhotoActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Intent i = getIntent();
+        photoPath = i.getStringExtra("photo_path");
+
+        Bitmap bitmap = Utils.getBitmap(photoPath);
+        ImageView imPhoto = findViewById(R.id.imPhoto);
+        imPhoto.setImageBitmap(bitmap);
+
     }
 
     public boolean onCreateOptionsMenu (Menu menu) {
@@ -38,6 +55,15 @@ public class PhotoActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    void sharePhoto() {
+        // Codigo para cpmpartiilhar a foto
+        Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this, "trindade.galeria.fileprovider", new File(photoPath));
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_STREAM, photoUri);
+        i.setType("image/jpeg");
+        startActivity(i);
     }
 
 }
